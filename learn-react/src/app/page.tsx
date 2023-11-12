ce1c945fddd06958f9634d60b7db768cd1ae8f6f
SarCraft <nathan.henaux30@gmail.com>
2023-11-12 13:09:15 +0100

    Learn basic react.js #2

--- learn-react/src/app/page.tsx
+++ learn-react/src/app/page.tsx
@@ -1,18 +1,42 @@
+'use client';
+
+import { useState } from "react";
+
+// Home Page
+
 export default function Home() {
+  // const [count, setCount] = useState(0); Permet de mettre les deux bouttons à jour en même temps
+
+  // function handleClick() {
+  //   setCount(count + 1);
+  // }
+
     return (
       <>
         <h1>Portfolio</h1>
-        <MyButton />
+        <MyButton /*count={count} onClick={handleClick} (Si l'on veut mettre les deux bouttons à jour, en même temps)*/  />
+        <MyButton /*count={count} onClick={handleClick}*/ />
         <Avatar />
       </>
     );
 }
 
+// Component MyButton
+
 function MyButton() {
+  const [count, setCount] = useState(0); // Disparait si l'on veut mettre les deux bouttons à jour, en même temps
+
+  function handleClick() {
+    setCount(count + 1);
+  }
   return (
-    <button className="btn">I'm a button</button>
+    <button onClick={/*onClick (Si l'on veut mettre les deux bouttons à jour, en même temps)*/handleClick} className="btn">
+      Clicked {count} times
+    </button>
   );
 }
+
+// Component Avatar
 
 function Avatar() {
   return (
